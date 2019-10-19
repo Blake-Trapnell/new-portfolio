071c3c80bf279933adcd0fe0b0d911d0d4bfa91f
Blake-Trapnell <52056662+Blake-Trapnell@users.noreply.github.com>
2019-10-19 14:32:10 -0600

    updated static

--- server/server.js
+++ server/server.js
@@ -4,5 +4,6 @@ const{SERVER_PORT} = process.env
 const PORT = SERVER_PORT || 4310
 const app = express()
 app.use(express.json())
+app.use( express.static( `${__dirname}/../build` ) );
 
 app.listen(PORT, ()=> console.log(PORT))
